d686e5991435457e3555f03325d22b59a6c69448
Kashinath Borker <borkerkashinath@gmail.com>
2023-01-23 14:44:17 +0530

    This is flask python app

---
+++ index.py
@@ -0,0 +1,12 @@
+from flask import Flask
+hello_world = Flask(__name__)
+
+@hello_world.route("/")
+
+def run():
+
+ return "{\"message\":\"Hello, World!\"}"
+
+if __name__== "__main__":
+    hello_world.run(host="0.0.0.0", port=int("3000"), debug= True)
+
